f3364d69c79e3d66042f192f305e4387ae66eba4
Furkan <furkanyilmaz-@hotmail.com>
2021-04-08 17:38:49 +0300

    Add project files.

---
+++ Teknokent_cizelge/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace Teknokent_cizelge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.ay_Sec = new System.Windows.Forms.Label();
+            this.yil_sec = new System.Windows.Forms.Label();
+            this.cizelge_button = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tc_girisbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(126, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(126, 150);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 27);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // ay_Sec
+            // 
+            this.ay_Sec.AutoSize = true;
+            this.ay_Sec.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ay_Sec.Location = new System.Drawing.Point(26, 110);
+            this.ay_Sec.Name = "ay_Sec";
+            this.ay_Sec.Size = new System.Drawing.Size(87, 19);
+            this.ay_Sec.TabIndex = 5;
+            this.ay_Sec.Text = "Ay Seçiniz :";
+            this.ay_Sec.UseWaitCursor = true;
+            // 
+            // yil_sec
+            // 
+            this.yil_sec.AutoSize = true;
+            this.yil_sec.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yil_sec.Location = new System.Drawing.Point(26, 155);
+            this.yil_sec.Name = "yil_sec";
+            this.yil_sec.Size = new System.Drawing.Size(85, 19);
+            this.yil_sec.TabIndex = 5;
+            this.yil_sec.Text = "Yıl Seçiniz :";
+            this.yil_sec.UseWaitCursor = true;
+            // 
+            // cizelge_button
+            // 
+            this.cizelge_button.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cizelge_button.Location = new System.Drawing.Point(124, 216);
+            this.cizelge_button.Name = "cizelge_button";
+            this.cizelge_button.Size = new System.Drawing.Size(121, 41);
+            this.cizelge_button.TabIndex = 6;
+            this.cizelge_button.Text = "Çizelge oluştur";
+            this.cizelge_button.UseVisualStyleBackColor = true;
+            this.cizelge_button.Click += new System.EventHandler(this.cizelge_button_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(124, 263);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.UseWaitCursor = true;
+            this.listBox1.Visible = false;
+            // 
+            // tc_girisbox
+            // 
+            this.tc_girisbox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tc_girisbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tc_girisbox.Location = new System.Drawing.Point(125, 70);
+            this.tc_girisbox.Multiline = true;
+            this.tc_girisbox.Name = "tc_girisbox";
+            this.tc_girisbox.Size = new System.Drawing.Size(121, 26);
+            this.tc_girisbox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(26, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "TC Giriniz :";
+            this.label1.UseWaitCursor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(251, 216);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 370);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tc_girisbox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cizelge_button);
+            this.Controls.Add(this.yil_sec);
+            this.Controls.Add(this.ay_Sec);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Çizelge";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label ay_Sec;
+        private System.Windows.Forms.Label yil_sec;
+        private System.Windows.Forms.Button cizelge_button;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox tc_girisbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Teknokent_cizelge/Form1.cs
@@ -0,0 +1,283 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LicenseContext = OfficeOpenXml.LicenseContext;
+using DataTable = System.Data.DataTable;
+
+
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+
+using Microsoft.Data.Sqlite;
+
+namespace Teknokent_cizelge
+{
+    public partial class Form1 : Form
+    {
+        SqliteConnection baglanti = new SqliteConnection(@"Data Source= C:\Users\furka\OneDrive\Masaüstü\software_eng\VisualStudioProjects\Teknokent_cizelge\Teknokent_cizelge\Datetime.db");
+
+        public void listele()
+        {
+            baglanti.Open();
+            SqliteCommand cek = new SqliteCommand();
+            cek.Connection = baglanti;
+            cek.CommandText = "Select Tc,Tarih, Baslangic_Saati, Bitis_Saati from olustur";
+            using (SqliteDataReader veri = cek.ExecuteReader())
+            {
+                System.Data.DataTable dt = new DataTable();
+                dt.Load(veri);
+                veri.Close();
+                baglanti.Close();
+                dataGridView1.DataSource = dt;
+            }
+        }
+        public void Olustur(int i ,string ay, string yil)
+        {
+            
+            baglanti.Open();
+            string ekle = "insert into olustur(id,Tc,Tarih,Baslangic_Saati,Bitis_Saati) values (@id,@Tc,@Tarih,@Baslangic_Saati,@Bitis_Saati)";
+            SqliteCommand gonder = new SqliteCommand(ekle, baglanti);
+           gonder.Parameters.AddWithValue("@id", i);
+            gonder.Parameters.AddWithValue("@Tc", tc_girisbox.Text);
+            gonder.Parameters.AddWithValue("@Tarih", i + "." + ay + "." + yil);
+            gonder.Parameters.AddWithValue("@Baslangic_Saati", "08:00" );
+            gonder.Parameters.AddWithValue("@Bitis_Saati","17:00" );
+
+            gonder.ExecuteNonQuery();  //?????????
+            baglanti.Dispose();
+            gonder.Dispose();
+            //MessageBox.Show("Aktarım Tamamlandı");
+        }
+        public void Sil(int tc)
+        {
+            baglanti.Open();
+            string sil = "delete from olustur where id='" + tc + "'";
+            SqliteCommand temizle = new SqliteCommand(sil, baglanti);
+            temizle.ExecuteNonQuery();
+            baglanti.Close();
+            listele();
+
+        }
+        public static int Gunbul (int ay, int yil)
+        {
+            switch (ay)
+            {
+                case 1:
+                    return 31;
+                    break;
+                case 2:
+                    if (yil % 4 == 0)
+                    {
+                        return 29;
+                    }
+                    else
+                    {
+                        return 28;
+                        break;
+                    }
+     
+                case 3:
+                    return 31;
+                    break;
+                case 4:
+                    return 30;
+                    break;
+                case 5:
+                    return 31;
+                    break;
+                case 6:
+                    return 30;
+                    break;
+                case 7:
+                    return 31;
+                    break;
+                case 8:
+                    return 31;
+                    break;
+                case 9:
+                    return 30;
+                    break;
+                case 10:
+                    return 31;
+                    break;
+                case 11:
+                    return 30;
+                    break;
+                case 12:
+                    return 31;
+                    break;
+                default:
+                    return 1;
+
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+
+           
+            
+            baglanti.Open();
+
+            string sql = "select  *from aylar";
+
+            SqliteCommand komut = new SqliteCommand(sql, baglanti);
+            SqliteDataReader read = komut.ExecuteReader();
+
+            while (read.Read())
+            {
+                comboBox1.Items.Add(read["aylar_ad"]);
+            }
+
+            baglanti.Close();
+
+            baglanti.Open();
+
+            string sql1 = "select  *from yillar";
+
+            SqliteCommand komut2 = new SqliteCommand(sql1, baglanti);
+            SqliteDataReader read2 = komut2.ExecuteReader();
+
+            while (read2.Read())
+            {
+                comboBox2.Items.Add(read2["yil_ad"]);
+            }
+
+            baglanti.Close();
+
+          
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cizelge_button_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1 || comboBox2.SelectedIndex == -1)
+            {
+                MessageBox.Show("Lütfen Ay ve Yıl Seçiniz");
+            }
+            else
+            {
+                //İşlemler
+                listBox1.Items.Clear();
+
+                try
+                {
+                    for (int k = 1; k <= 31; k++)
+                    {
+                        Sil(k);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Hata: ");
+                }
+
+
+                int a = comboBox1.SelectedIndex;
+                int b = Convert.ToInt32(comboBox2.SelectedItem);
+                //DateTime Ay = DateTime.Now.AddMonths(a);  //Ay Ekleme
+                //DateTime Yil = DateTime.Now.AddYears(b);  //Yıl Ekleme
+                string ay = Convert.ToString(comboBox1.SelectedIndex);
+
+
+               
+
+                for (int i = 1; i <= Gunbul(a, b); i++)
+                {
+
+                    DateTime dt = new DateTime(b, a, i);
+                    int kacinciGun = (int)dt.DayOfWeek;
+
+                    if (kacinciGun == 6 || kacinciGun == 0)
+                    {
+                        if (kacinciGun == 0)
+                        {
+                            listBox1.Items.Add(i + " / " + comboBox1.SelectedItem + " / " + b + " PAZAR" + " Hafta Sonu");
+                            //  Olustur(i, ay, b.ToString(), "PAZAR", "Hafta Sonu");
+                        }
+                        else
+                        {
+                            listBox1.Items.Add(i + " / " + comboBox1.SelectedItem + " / " + b + " CUMARTESİ" + " Hafta Sonu");
+                            //Olustur(i, ay, b.ToString(), "CUMARTESİ", "Hafta Sonu");
+                        }
+
+                    }
+
+                    else
+                    {
+                        switch (kacinciGun)
+                        {
+                            case 1:
+                                listBox1.Items.Add(tc_girisbox.Text + i+ " / " + comboBox1.SelectedIndex + " / " + b + " PAZARTESİ" + " Hafta İçi");
+                                Olustur(i, ay, b.ToString());
+                                break;
+                            case 2:
+                                listBox1.Items.Add(tc_girisbox.Text + i + " / " + comboBox1.SelectedItem + " / " + b + " SALI" + " Hafta İçi");
+                                Olustur(i, ay, b.ToString());
+                                break;
+                            case 3:
+                                listBox1.Items.Add(tc_girisbox.Text + i + " / " + comboBox1.SelectedItem + " / " + b + " ÇARŞAMBA" + " Hafta İçi");
+                                Olustur(i, ay, b.ToString());
+                                break;
+                            case 4:
+                                listBox1.Items.Add(tc_girisbox.Text + i + " / " + comboBox1.SelectedItem + " / " + b + " PERŞEMBE" + " Hafta İçi");
+                                Olustur(i, ay, b.ToString());
+                                break;
+                            case 5:
+                                listBox1.Items.Add(tc_girisbox.Text + i + " / " + comboBox1.SelectedItem + " / " + b + " CUMA" + " Hafta İçi");
+                                Olustur(i, ay, b.ToString());
+                                break;
+                        }
+
+                    }
+                }
+                listele();
+
+                Excel.Application app = new Excel.Application();
+                // app.Visible = true;
+                Workbook kitap = app.Workbooks.Add(System.Reflection.Missing.Value);
+                object misValue = System.Reflection.Missing.Value;
+                Worksheet sayfa = (Worksheet)kitap.Sheets[1];
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    Range alan = (Range)sayfa.Cells[1, 1];
+                    alan.Cells[1, i + 1] = dataGridView1.Columns[i].HeaderText;
+                }
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                    {
+                        Range alan2 = (Range)sayfa.Cells[j + 1, i + 1];
+                        alan2.Cells[2, 1] = dataGridView1[i, j].Value;
+                    }
+                }
+               
+                kitap.SaveAs("C:\\cizelgeler\\" + tc_girisbox.Text + ".xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                kitap.Close(true, misValue, misValue);
+                app.Quit();
+
+                MessageBox.Show("Çizelge kaydedildi");
+            }
+            
+           
+
+          
+        }
+    }
+}
